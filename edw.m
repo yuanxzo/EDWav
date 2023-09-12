896eb668cc0b441ddb9508d13f46654085fcaecd
yuanxzo <seism.yang@foxmail.com>
2023-09-12 08:50:16 +0800

    Update edw.m

--- edw.m
+++ edw.m
@@ -65,7 +65,7 @@
 %       [1] Bo Yang, Haoran Meng, Ning Gu, Xin Liu, Xiaofei Chen, and
 %           Yehuda Ben-Zion. 2023. A Frequency Domain Methodology for
 %           Quantitative Evaluation of Diffuse Wavefield with Applications
-%           to Seismic Imaging. In preparation.
+%           to Seismic Imaging. Preprint.
 % =========================================================================
 
 classdef edw
