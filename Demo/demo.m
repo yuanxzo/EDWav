6a840bab48b6296a1661532c783368343c063a9c
yuanxzo <seism.yang@foxmail.com>
2023-09-12 08:50:46 +0800

    Update demo.m

--- Demo/demo.m
+++ Demo/demo.m
@@ -96,4 +96,4 @@ disp(new.isdiff)
 %   [1] Bo Yang, Haoran Meng, Ning Gu, Xin Liu, Xiaofei Chen, and Yehuda
 %       Ben-Zion. 2023. A Frequency Domain Methodology for Quantitative
 %       Evaluation of Diffuse Wavefield with Applications to Seismic
-%       Imaging. In preparation.
+%       Imaging. Preprint.
