df13a9c60b853729a7557a0aec047884057543ca
yuanxzo <seism.yang@foxmail.com>
2025-03-24 20:04:44 +0800

    Update EDWav.py

--- Python Version/EDWav.py
+++ Python Version/EDWav.py
@@ -4,6 +4,10 @@ Author: Bo YANG (seism.yang@foxmail.com)
 Reference: Yang, B., Meng, H., Gu, N., Liu, X., Chen, X., & Ben‐Zion, Y. (2024). 
            A frequency domain methodology for quantitative evaluation of diffuse wavefield with applications to seismic imaging. 
            Journal of Geophysical Research: Solid Earth, 129, e2024JB028895. https://doi.org/10.1029/2024JB028895
+
+           Yang, B., Meng, H., Yuan, S., & Chen, X. (2025). 
+           Reliable Multimodal Attenuation Estimation of Surface Waves Using Diffuse Ambient Noise: Theory and Applications. 
+           Preprints. https://doi.org/10.22541/essoar.174120292.25931294/v1
 Latest: 2024-05-14   
 
 Copyright (C) 2024 Bo YANG
