e2083d984fc30c15c544a60488e2e3bb74f67f0b
yuanxzo <seism.yang@foxmail.com>
2025-09-28 14:28:19 +0800

    Update EDWav.py

--- Python Version/EDWav.py
+++ Python Version/EDWav.py
@@ -1,13 +1,14 @@
 """
 Name  : The Method of Evaluation of the Diffuseness of the Wavefield (EDWav Method)
 Author: Bo YANG (seism.yang@foxmail.com)
-Reference: Yang, B., Meng, H., Gu, N., Liu, X., Chen, X., & Ben‐Zion, Y. (2024). 
-           A frequency domain methodology for quantitative evaluation of diffuse wavefield with applications to seismic imaging. 
-           Journal of Geophysical Research: Solid Earth, 129, e2024JB028895. https://doi.org/10.1029/2024JB028895
+Reference: 
+       [1] Yang, B., Meng, H., Gu, N., Liu, X., Chen, X., & Ben-Zion, Y. (2024). 
+           A Frequency Domain Methodology for Quantitative Evaluation of Diffuse Wavefield With Applications to Seismic Imaging. 
+           Journal of Geophysical Research: Solid Earth, 129(5), e2024JB028895. https://doi.org/10.1029/2024JB028895
 
-           Yang, B., Meng, H., Yuan, S., & Chen, X. (2025). 
+       [2] Yang, B., Meng, H., Yuan, S., & Chen, X. (2025). 
            Reliable Multimodal Attenuation Estimation of Surface Waves Using Diffuse Ambient Noise: Theory and Applications. 
-           Preprints. https://doi.org/10.22541/essoar.174120292.25931294/v1
+           Journal of Geophysical Research: Solid Earth, 130(9), e2025JB031418. https://doi.org/10.1029/2025JB031418
 Latest: 2025-03-24   
 
 Copyright (C) 2024 Bo YANG
@@ -450,3 +451,4 @@ def add_right_cax(ax, pad, width):
     cax = ax.figure.add_axes(caxpos)
 
     return cax
+
